cde0bc0f4838ce74d7132c06186a6c9ad44efa18
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-30 19:45:46 +0300

    I have added the constructor to the contact class

---
+++ contact.py
@@ -0,0 +1,10 @@
+#build the contact class
+class contact:
+   def _init_(self,firstName,secondName,number,email):
+       #class variables
+       contact_list=[]
+       #instance variables
+       self.firstName=firstName
+       self.secondName=secondName
+       self.number=number
+       self.email=email
