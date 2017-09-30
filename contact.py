5674e84d95d19df56b2822169b72811686e5ebdb
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-30 20:51:29 +0300

    I have added the method to add the contact object into the list and the test run well

--- contact.py
+++ contact.py
@@ -6,6 +6,11 @@ class Contact:
 
     contact_list = [] # Empty contact list
 
+    #method to save contacts to the list
+    def saveContact(self):
+        #appends the object to the list
+        Contact.contact_list.append(self)
+
     def __init__(self,first_name,last_name,number,email):
 
       # docstring removed for simplicity
