4aaa4c7cce3d43362971ab6d7330fa0fe9753356
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-02 23:11:03 +0300

    I created the contact using the parameters and saved it

--- run.py
+++ run.py
@@ -37,6 +37,23 @@ def main():
 
     while True:
         print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+        short_code=input().lower()
+        if short_code =='cc':
+            print("New Contact")
+            print("-"*10)
+
+            print ("First name ....")
+            f_name = input()
+
+            print("Last name ...")
+            l_name = input()
+
+            print("Phone number ...")
+            p_number = input()
+
+            print("Email address ...")
+            e_address = input()
+            saveContact(createContact(f_name,l_name,p_number,e_address))#createcontact then save it
 
 if __name__ == '__main__':
 
