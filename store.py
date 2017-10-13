2e9f6cdbf2d1fbcc23f7549cd08b638b5cdf5bcc
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-13 08:08:16 +0300

    I have added the loop to get the name,price and stock available

--- store.py
+++ store.py
@@ -1,2 +1,10 @@
 prices={"banana": 4,"apple": 2,"orange":1.5,"pear":3}
 stock={"banana": 6,"apple": 0,"orange": 32,"pear": 15}
+#loop through the dictionaries and print name ,price and stock
+for key in prices:
+
+  print key
+
+  print "price: %s" % prices[key]
+
+  print "stock: %s" % stock[key]
