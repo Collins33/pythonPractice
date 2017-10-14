1cf8739c56e2a8bf8fb5401e0344bef893d2adef
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-14 13:09:43 +0300

    I have added ability to add an item if its stock is greater than zero and then reduce its amount by one

--- store.py
+++ store.py
@@ -3,25 +3,30 @@ stock={"banana": 6,"apple": 0,"orange": 32,"pear": 15}
 #loop through the dictionaries and print name ,price and stock
 for key in prices:
 
-  print key
+  print (key)
 
-  print "price: %s" % prices[key]
+  print ("price: %s" % prices[key])
 
-  print "stock: %s" % stock[key]
+  print ("stock: %s" % stock[key])
 
 #get how much our store is worth by multiplying total stock and total price
 total=0
 for key in prices:
   amount=prices[key]*stock[key]
   total +=amount
-  print total
+  print (total)
 
 
 #grocery list
-groceries=['banana','orange','apple']
-
+shopping_list=['banana','orange','apple']
+#loop through the list
+#then get the individual item and use it as a key to get access to the dictionary of keys
+#then use their prices to get the total amount
 def compute_bill(food):
   total = 0
   for item in food:
-    total = total + prices[item]
+    if stock[item] >0:
+      total = total + prices[item]
+      stock[item]=stock[item] - 1
   return total
+print (compute_bill(shopping_list))
